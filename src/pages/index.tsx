980d32c794696ac37a8e443ac2aa9aaa3a93b35a
William Rocha <william.bueno.rocha@gmail.com>
2022-01-29 20:59:08 -0300

    layout and title components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className="">
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout title='Cadastro simples'>
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
