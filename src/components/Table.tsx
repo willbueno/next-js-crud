210bdde096ec4e1b5b77414ff999bac3ce479f73
William Rocha <william.bueno.rocha@gmail.com>
2022-01-29 21:40:29 -0300

    table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,77 @@
+import Client from "../core/Client";
+import { EditIcon, TrashIcon } from "./icons";
+
+interface TableProps {
+    clients: Client[]
+    selectedClient?: (client: Client) => void
+    deletedClient?: (client: Client) => void
+}
+
+export default function Table(props: TableProps) {
+
+    const showActions = props.selectedClient || props.deletedClient
+
+    function renderHeader() {
+        return (
+            <tr>
+                <th className="text-left p-4">Codigo</th>
+                <th className="text-left p-4">Nome</th>
+                <th className="text-left p-4">Idade</th>
+                {showActions ? <th className="p-4">Ações</th> : false}
+            </tr>
+        )
+    }
+
+    function renderData() {
+        return props.clients?.map((client, i) => {
+            return (
+                <tr key={client.id}
+                    className={`${i % 2 == 0 ? 'bg-purple-200' : 'bg-purple-100'}`}>
+                    <td className="text-left p-4">{client.id}</td>
+                    <td className="text-left p-4">{client.name}</td>
+                    <td className="text-left p-4">{client.age}</td>
+                    {showActions ? renderActions(client) : false}
+                </tr>
+            )
+        })
+    }
+
+    function renderActions(client: Client) {
+        return (
+            <td className="flex">
+                {props.selectedClient ? (
+                    <button onClick={() => props.selectedClient?.(client)} className={`
+                        flex justify-center items-center
+                        text-green-600 rounded-full p-2 m-1
+                        hover:bg-purple-50
+                    `}>
+                        {EditIcon}
+                    </button>
+                ) : false}
+                {props.deletedClient ? (
+                    <button onClick={() => props.deletedClient?.(client)} className={`
+                        flex justify-center items-center
+                        text-red-500 rounded-full p-2 m-1
+                        hover:bg-purple-50
+                    `}>
+                        {TrashIcon}
+                    </button>
+                ) : false}
+            </td>
+        )
+    }
+
+    return (
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+                text-gray-100
+                bg-gradient-to-r from-purple-500 to to-purple-800
+            `}>
+                {renderHeader()}
+            </thead>
+            <tbody>
+                {renderData()}
+            </tbody>
+        </table>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+  const clients = [
+    new Client('Ana', 32, '1'),
+    new Client('Bia', 45, '2'),
+    new Client('Carlos', 23, '3'),
+    new Client('Pedro', 54, '4')
+  ]
+
+  function selectedClient(client: Client) {
+    console.log(client.name)
+  }
+
+  function deletedClient(client: Client) {
+    console.log(`Excluir... ${client.name}`)
+  }
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +25,7 @@ export default function Home() {
       text-white
     `}>
       <Layout title='Cadastro simples'>
-        <span>Conteudo</span>
+        <Table clients={clients} selectedClient={selectedClient} deletedClient={deletedClient} />
       </Layout>
     </div>
   )
