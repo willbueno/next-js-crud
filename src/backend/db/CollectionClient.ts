0c5a02c48382c75c5fe2953bc9f1eb40ac9022d7
William Rocha <william.bueno.rocha@gmail.com>
2022-01-30 00:05:47 -0300

    clients repository

---
+++ src/backend/db/CollectionClient.ts
@@ -0,0 +1,45 @@
+import firebase from "../config";
+import Client from "../../core/Client";
+import ClientRepository from "../../core/ClientRepository";
+
+export default class CollectionClient implements ClientRepository {
+
+    #converter = {
+        toFirestore(client: Client) {
+            return {
+                name: client.name,
+                age: client.age
+            }
+        },
+        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Client {
+            const data = snapshot.data(options)
+            return new Client(data.name, data.age, snapshot.id)
+        }
+    }
+
+    async save(client: Client): Promise<Client> {
+        if (client?.id) {
+            await this.#collection().doc(client.id).set(client)
+            return client
+        } else {
+            const docRef = await this.#collection().add(client)
+            const doc = await docRef.get()
+            return doc.data()
+        }
+    }
+
+    async delete(client: Client): Promise<void> {
+        return this.#collection().doc(client.id).delete()
+    }
+
+    async getAll(): Promise<Client[]> {
+        const query = await this.#collection().get()
+        return query.docs.map(doc => doc.data()) ?? []
+    }
+
+    #collection() {
+        return firebase
+            .firestore().collection('Clients')
+            .withConverter(this.#converter)
+    }
+}
